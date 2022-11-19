a48fcccf1e3ae35b99cb1e2c43ceffa48469d6f7
Euan Morgan <euan@cacode.co.uk>
2022-11-19 12:56:19 +0000

    feat: setup swagger

--- src/utils/createServer.ts
+++ src/utils/createServer.ts
@@ -2,10 +2,27 @@ import fastify from "fastify";
 import { todoRoute } from "../modules/todo/todo.route";
 import { config } from "./config";
 import { envToLogger } from "./logger";
-
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
+import { version } from "../../package.json";
 export const createServer = async () => {
 	const app = fastify({
 		logger: envToLogger[config.NODE_ENV],
+	});
+
+	app.register(swagger, {
+		swagger: {
+			info: {
+				title: "Todo API",
+				description: "Todo API",
+				version,
+			},
+		},
+	});
+
+	app.register(swaggerUi, {
+		routePrefix: "/docs",
+		staticCSP: true,
 	});
 
 	app.register(todoRoute, { prefix: "/api/todos" });
